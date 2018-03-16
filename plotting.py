dba62bff9952a47f21a06805d8c21ad3a43d5460
Jeff Hara <jhara_mac@DN0a25b759.SUNet>
2018-03-16 16:20:05 -0700

    added plotting.py with function that draws bboxes on the image modified version of what we had in the homework

---
+++ plotting.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+
+colors = ['red', 'blue', 'green', 'yellow']
+
+# Displays the HoG features next to the original image
+def plot_img_with_bbox(im, bboxes, title_text = None):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    for color, feat in zip(colors, bboxes):
+        for _, bbox in bboxes[feat]:
+            ax.add_patch(
+                patches.Rectangle(
+                    (bbox[0], bbox[1]),
+                    bbox[2],
+                    bbox[3],
+                    fill=False,
+                    edgecolor=color
+                )
+            )
+
+    plt.imshow(im, 'gray')
+    if title_text is not None:
+        plt.title(title_text)
