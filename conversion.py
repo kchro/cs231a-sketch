04d17da39b378b93f54e8aa0b0396b5ac2455185
Jeff Hara <jhara_mac@DN0a25b759.SUNet>
2018-03-18 13:21:44 -0700

    added new function get_window_3_stroke that generates a small window of the 3-strokes

--- conversion.py
+++ conversion.py
@@ -30,7 +30,7 @@ def lines_to_strokes(lines):
             eos = 0 if i < n-1 else 1
             strokes.append([line[i][0], line[i][1], eos])
 
-    strokes = np.array(strokes)
+    strokes = np.array(strokes, dtype=np.float32)
 
     # calculate the delta
     strokes[1:, 0:2] -= strokes[:-1, 0:2]
@@ -66,6 +66,8 @@ def convert_to_3_stroke(im):
     bmp = potrace.Bitmap(data)
     path = bmp.trace()
 
+    plt.imshow(data, cmap=plt.cm.gray)
+
     # get the xy coordinates for each curve
     lines = []
     for i, curve in enumerate(path):
@@ -75,17 +77,84 @@ def convert_to_3_stroke(im):
             continue
 
         line = curve.tesselate()
+
         # perform Ramer-Douglas-Peuker algorithm
-        line = rdp(line, epsilon=2)
+        line = rdp(line, epsilon=1)
+
+        x, y = line.T
+        plt.plot(x, y, c='red')
 
         lines.append(line)
+
+    plt.show()
 
     # get the 3 stroke format
     strokes = lines_to_strokes(lines)
 
     return strokes
 
+def get_curves(im, kernel_size=3):
+    """
+    params:
+        im          image
+    returns:
+        lines       xy coordinates of lines
+    """
+    # black background, white sketch
+    _, thresh = cv2.threshold(im, 200, 255, cv2.THRESH_BINARY_INV)
+
+    # dilate -> erode
+    kernel = np.ones((kernel_size,kernel_size), np.uint8)
+    im2 = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
+
+    # Get inverted boolean matrix:
+    # potrace only works with boolean images
+    data = im2 == 0
+
+    # Create a bitmap from the array
+    bmp = potrace.Bitmap(data)
+    paths = bmp.trace()
+
+    lines = []
+    for i, curve in enumerate(paths):
+        if i == 0:
+            continue
+        line = curve.tesselate()
+        line = rdp(line, epsilon=1)
+        lines.append(line)
+
+    return lines
+
+def get_window_3_stroke(lines, im, j, i, window_size=100):
+    # plt.imshow(im[j:j+window_size, i:i+window_size],
+    #            cmap=plt.cm.gray)
+
+    new_lines = []
+    for line in lines:
+        # print line
+
+        inbounds = []
+        for x, y in line:
+            if (x > i and x < i+window_size) and \
+               (y > j and y < j+window_size):
+
+               inbounds.append([x, y])
+        inbounds = np.array(inbounds)
+
+        if len(inbounds) > 0:
+            new_lines.append(inbounds)
+            # X, Y = inbounds.T
+            # plt.plot(X, Y, c='red')
+
+    # plt.show()
+
+    strokes = lines_to_strokes(new_lines)
+    return strokes
+
 if __name__ == '__main__':
-    for face in load_faces(n=10):
+    for face in load_faces(n=5):
         im = cv2.imread(face, 0)
-        strokes = convert_to_3_stroke(im)
+        # strokes = convert_to_3_stroke(im)
+        lines = get_curves(im)
+        lines = get_window_3_stroke(lines, im, 0, 0)
+        # draw_strokes(strokes)
