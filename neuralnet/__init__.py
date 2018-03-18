5af5361744bc9b331b628bf2db2584deba027e82
Jeff Hara <jhara_mac@jhara.local>
2018-03-17 20:15:16 -0700

    updated __init__.py: call load_trained_classifier in main functions in root folder

--- neuralnet/__init__.py
+++ neuralnet/__init__.py
@@ -6,24 +6,24 @@ from model_class import Model
 
 #load pathways
 data_dir = 'neuralnet/datasets/'
-models_root_dir = 'neuralnet/' #same directory we're in now
+models_root_dir = '' #same directory we're in now
 model_dir = 'neuralnet/checkpoint_path/classifier'
 
 def load_trained_classifier():
     print 'loading datasets...'
-    _, _, _, _, eval_hps_model, _ = load_env(data_dir, model_dir)
+    _, _, test_set, _, eval_hps_model, _ = load_env(data_dir, model_dir)
     print 'loaded datasets.'
-    
+
     # construct the sketch-rnn model:
     reset_graph()
-    eval_model = Model(eval_hps_model, reuse=True)
-    
+    eval_model = Model(eval_hps_model, reuse=tf.AUTO_REUSE)
+
     # run session
     sess = tf.InteractiveSession()
     sess.run(tf.global_variables_initializer())
-    
+
     # load checkpoints
     load_checkpoint(sess, model_dir)
-    
+
     # return session and evaluation model
-    return sess, eval_model
+    return sess, eval_model, test_set
