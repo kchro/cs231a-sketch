7a384ace320b232569b235ffaafee33be82c463a
Jeff Hara <jhara_mac@DN0a25b8e8.SUNet>
2018-03-20 18:17:23 -0700

    updated classify

--- classify.py
+++ classify.py
@@ -4,51 +4,64 @@ import matplotlib.pyplot as plt
 import cv2
 from face_data import load_faces
 from plotting import plot_img_with_bbox
+from conversion import get_window_3_stroke
+from neuralnet import FeatureClassifier
 
-def get_bounding_boxes(im, window_size=100, shift_size=50, thresh=0.3):
+# property of the neural net classifier
+MAX_LEN = 125
+labels = ['ear', 'eye', 'mouth', 'nose']
+
+def get_bounding_boxes(im,
+                       model,
+                       window_size=100,
+                       shift_size=50,
+                       scale_factor=10,
+                       thresh=0.5,
+                       verbose=False):
     """
     params:
         im              image (256x256)
+        model           Feature Classifier
         window_size     size of bounding box
         shift_size      size of window shift
+        scale_factor    scale of strokes
         thresh          minimum confidence
     """
     H, W = im.shape
     bboxes = {}
 
-    def get_window(j, i):
-        return im[j:j+window_size, i:i+window_size]
-
+    shape = (window_size, window_size)
     for j in range(0, H-window_size, shift_size):
         for i in range(0, W-window_size, shift_size):
-            window = get_window(j, i)
+            stroke = get_window_3_stroke(im, j, i,
+                                         window_shape=shape,
+                                         scale_factor=scale_factor)
 
-            # pred_class, confidence = model.predict(window)
-            raise NotImplementedError
+            pred_class, confidence = model.predict(stroke)
+            if verbose:
+                print pred_class, confidence
+                model.draw(stroke)
 
-            if pred_class == None or \
-               confidence < thresh:
-                # no bounding box
+            if pred_class == None:
+                if verbose:
+                    print 'no classification'
                 continue
 
-            if pred_class:
-                if pred_class not in bboxes:
-                    bboxes[pred_class] = []
-                bbox = (j, i, window_size, window_size)
-                bboxes[pred_class].append((confidence, bbox))
+            if pred_class not in bboxes:
+                bboxes[pred_class] = []
+            bbox = (j, i, window_size, window_size)
+            bboxes[pred_class].append((confidence, bbox))
 
     return bboxes
 
-def non_maximal_suppression(bboxes):
+def non_maximal_suppression(bboxes, eyes=2, noses=1, mouths=1):
     for feat in bboxes:
         if feat == 'eye':
-            top_bboxes = sorted(bboxes[feat])[-2:]
+            top_bboxes = sorted(bboxes[feat])[-eyes:]
         elif feat == 'nose':
-            top_bboxes = sorted(bboxes[feat])[-1:]
+            top_bboxes = sorted(bboxes[feat])[-noses:]
         elif feat == 'mouth':
-            top_bboxes = sorted(bboxes[feat])[-1:]
-        elif feat == 'ear':
-            top_bboxes = sorted(bboxes[feat])[-2:]
+            top_bboxes = sorted(bboxes[feat])[-mouths:]
 
         bboxes[feat] = top_bboxes
 
@@ -58,6 +71,8 @@ if __name__ == "__main__":
     # get a face image
     faces = load_faces(n=10)
 
+    model = FeatureClassifier()
+
     # for each face
     for face in faces:
         # zero for grayscale
@@ -65,13 +80,11 @@ if __name__ == "__main__":
         cv2.imshow('image', im)
         cv2.waitKey(0)
 
-        continue
-
         # slide a window over the face and get bounding boxes
-        bboxes = get_bounding_boxes(im)
+        bboxes = get_bounding_boxes(im, model)
 
         # non-maximal suppression
-        bboxes = non_maximal_suppression(bboxes)
+        # bboxes = non_maximal_suppression(bboxes)
 
         plot_img_with_bbox(im, bboxes)
         plt.show()
--- plotting.py
+++ plotting.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 
-colors = ['red', 'blue', 'green', 'yellow']
+colors = ['red', 'blue', 'green']
 
 # Displays the HoG features next to the original image
 def plot_img_with_bbox(im, bboxes, title_text = None):
@@ -23,3 +23,21 @@ def plot_img_with_bbox(im, bboxes, title_text = None):
     plt.imshow(im, 'gray')
     if title_text is not None:
         plt.title(title_text)
+
+# simple plotting for debugging purposes
+def plot_bbox(im, bboxes):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    for bbox in bboxes:
+        ax.add_patch(
+            patches.Rectangle(
+                (bbox[0], bbox[1]),
+                bbox[2],
+                bbox[3],
+                fill=False,
+                edgecolor='red'
+            )
+        )
+
+    plt.imshow(im, 'gray')
